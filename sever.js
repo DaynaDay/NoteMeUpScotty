44b8ec4965846d138dbffd87baf6ee1f24784b14
Dayna Knight <dknighted11@gmail.com>
2020-02-20 12:33:13 -0500

    set up majority of my get requests in the server file.

--- sever.js
+++ sever.js
@@ -9,12 +9,25 @@ app.use(express.json());
 
 // GET /notes - Should return the notes.html file.
 
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
 // GET * - Should return the index.html file
+
+app.get("/index", function(req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 
 
 // GET /api/notes - Should read the db.json file and return all saved notes as JSON.
 
+app.get('/api/notes', function(req, res) {
+  res.json();
+});
+
 // POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+
 
 
 require("./routes/apiRoutes")(app);
