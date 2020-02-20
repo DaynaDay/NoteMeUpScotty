09a89cfa4b708d50e9701bbfc1789488a9a6ec7f
Dayna Knight <dknighted11@gmail.com>
2020-02-20 12:22:41 -0500

    set up the server page and added the api and html routes.

--- sever.js
+++ sever.js
@@ -1,1 +1,27 @@
 const express = require("express");
+
+var app = express();
+var PORT = 3000;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// GET /notes - Should return the notes.html file.
+
+// GET * - Should return the index.html file
+
+
+// GET /api/notes - Should read the db.json file and return all saved notes as JSON.
+
+// POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
